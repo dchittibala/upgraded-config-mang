95e263d2dd3dd37e87a75b6e9d9b46a27baa7cd3
reddydinesh427 <reddydinesh427@gmail.com>
2023-09-01 00:20:07 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
+export PATH=$PATH:/usr/local/go/bin
+which go
+if [ $? -eq 0 ]; then
+        echo "Go is installed"
+        export PATH=$PATH:/usr/local/go/bin
+else
+curl -OL https://golang.org/dl/go1.21.0.linux-amd64.tar.gz; tar -C /usr/local -xvf go1.21.0.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+fi
+apt update; apt install git
+git clone https://github.com/reddydinesh427/upgraded-config-mang.git
+cd upgraded-config-mang
 
-
-wget -qO- https://github.com/reddydinesh427/upgraded-config-mang/releases/download/v1.0.0/provisioner.tar | tar xvz -C /tmp/provisioner
-
-mkdir /etc/upgraded-config-mang
-mv /tmp/provisioner/ /etc/upgraded-config-mang/
-
-chmod +x /etc/upgraded-config-mang/upgraded-config-mang-linux
-
-cd /etc/upgraded-config-mang/
-
-./upgraded-config-mang-linux -tasks ./tasks.json
+go run main.go -tasks tasks.json
