0b2d11ef4a7d2a3cdafbe5a324095790bc5a9176
dchittibala <dchittibala@apple.com>
2023-08-31 23:33:04 -0500

    changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,15 @@
 #! /bin/bash
 
+curl -o /tmp/provisioner.tar  https://github.com/reddydinesh427/upgraded-config-mang/releases/download/v1.0.0/provisioner.tar
 
-if [ "$(uname)" == "Darwin" ]; then
-    # Mac OS X platform
-    which go
-    if [ $? -eq 0 ]
-    then
-        echo "Go is already installed."
-        exit 0
-    else
-        brew install go
-    fi    
-         
-elif [ "$(uname -s)" == "Linux" ]; then
-    # Do something under GNU/Linux platform
-    which js
-    if [ $? -eq 0 ]
-    then
-        echo "Go is already installed."
-        exit 0
-    else
-        wget https://dl.google.com/go/go1.21.0.linux-amd64.tar.gz
-        tar -C /usr/local/ -xzf go1.21.0.linux-amd64.tar.gz
-        export PATH=$PATH:/usr/local/go/bin
-    fi
-else
-    echo "This can be built only on linux or darwin systems"
-fi
+tar -xf /tmp/provisioner.tar
+rm -rf /tmp/provisioner.tar
+
+mkdir /etc/upgraded-config-mang
+mv /tmp/provisioner/ /etc/upgraded-config-mang/
+
+chmod +x /etc/upgraded-config-mang/upgraded-config-mang-linux
+
+cd /etc/upgraded-config-mang/
+
+./upgraded-config-mang-linux -tasks ./tasks.json
